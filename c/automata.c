42cf245d17a9be2c8aa2adcd6c73d7cd2495022b
Rodrigo Arias <rodarima@gmail.com>
2014-10-07 17:06:57 +0200

    Parsear estados y alfabeto

--- c/automata.c
+++ c/automata.c
@@ -1,4 +1,5 @@
 #include "automata.h"
+#include "dbg.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,7 +24,7 @@ void afd_free(struct afd *afd)
 	free(afd);
 }
 
-int afd_readfd(struct afd *afd, FILE *f)
+int afd_fread(struct afd *afd, FILE *f)
 {
 	if(fgets(afd->buffer, MAX_LINE, f) == NULL)
 	{
@@ -34,14 +35,100 @@ int afd_readfd(struct afd *afd, FILE *f)
 	return 0;
 }
 
+static void afd_print_str(void *s)
+{
+	printf("%s\n", (char *) s);
+}
+
+static int afd_parse_list(struct list_t *l, char *str)
+{
+	char *p, *save;
+	debug("afd_parse_list(, %s)", str);
+	p = strtok_r(str, " ", &save);
+
+	while(p != NULL)
+	{
+		debug("Nuevo elemento %s", p);
+		if(list_add(l, p) != 0)
+		{
+			debug("list_add");
+			return 1;
+		}
+
+		p = strtok_r(NULL, " ", &save);
+	}
+
+	list_map(l, afd_print_str);
+	return 0;
+}
+
+static int afd_parse_sti(struct afd *afd, char *str)
+{
+	char *p, *save;
+	p = strtok_r(str, " ", &save);
+
+	if(p == NULL)
+	{
+		debug("Estado inicial no encontrado");
+		return 1;
+	}
+
+	afd->sti = p;
+
+	return 0;
+}
+
+int afd_parse(struct afd *afd)
+{
+	char *p, *save;
+	p = strtok_r(afd->buffer, ";", &save);
+
+	debug("Estados totales %s", p);
+
+	if(afd_parse_list(&afd->st, p) != 0)
+	{
+		debug("afd_parse_list");
+		return 1;
+	}
+	
+	p = strtok_r(NULL, ";", &save);
+
+	if(afd_parse_list(&afd->alfa, p) != 0)
+	{
+		debug("afd_parse_list");
+		return 1;
+	}
+	
+	p = strtok_r(NULL, ";", &save);
+
+	afd_parse_sti(afd, p);
+	
+	p = strtok_r(NULL, ";", &save);
+
+	if(afd_parse_list(&afd->stf, p) != 0)
+	{
+		debug("afd_parse_list");
+		return 1;
+	}
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	struct afd *afd;
 	
 	// TODO comprobaciones
 	afd_init(&afd);
-	afd_readfd(afd, stdin);
-	printf("%s\n", afd->buffer);
+	afd_fread(afd, stdin);
+	debug("Buffer %s", afd->buffer);
+
+	if(afd_parse(afd) != 0)
+	{
+		debug("afd_parse");
+		return 1;
+	}
+
 	afd_free(afd);
 
 	return 0;
--- c/automata.h
+++ c/automata.h
@@ -3,28 +3,18 @@
 
 #define MAX_LINE 1024
 
-struct afd_st
-{
-	char *name;
-};
-
-struct afd_sym
-{
-	char *name;
-};
-
 struct afd_tran
 {
-	struct afd_st ini;
-	struct afd_st fin;
-	struct afd_sym sym;
+	char *ini;
+	char *fin;
+	char *sym;
 };
 
 struct afd
 {
 	struct list_t trans;
 	struct list_t st;
-	struct afd_st sti;
+	char *sti;
 	struct list_t stf;
 	struct list_t alfa;
 
