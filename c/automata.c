a7a6c37d693db867f7716848779a189d92ef07f2
Rodrigo Arias <rodarima@gmail.com>
2014-10-07 20:22:08 +0200

    Mostrar diagrama dot para el autómata inicial.

--- c/automata.c
+++ c/automata.c
@@ -1,3 +1,5 @@
+//#define NDEBUG
+
 #include "automata.h"
 #include "dbg.h"
 
@@ -5,6 +7,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+
 
 int afd_init(struct afd **afd)
 {
@@ -19,8 +22,18 @@ int afd_init(struct afd **afd)
 	return 0;
 }
 
+static void afd_free_tran(void *p)
+{
+	free(p);
+}
+
 void afd_free(struct afd *afd)
 {
+	list_clear(&afd->st);
+	list_clear(&afd->stf);
+	list_clear(&afd->alfa);
+	list_clear_func(&afd->trans, afd_free_tran);
+	
 	free(afd);
 }
 
@@ -35,10 +48,20 @@ int afd_fread(struct afd *afd, FILE *f)
 	return 0;
 }
 
+#ifndef NDEBUG
+
 static void afd_print_str(void *s)
 {
-	printf("%s\n", (char *) s);
+	debug("%s", (char *) s);
 }
+
+static void afd_print_tran(void *ptr)
+{
+	struct afd_tran *t = (struct afd_tran *) ptr;
+	debug("%s -- %s --> %s", t->ini, t->sym, t->fin);
+}
+
+#endif
 
 static int afd_parse_list(struct list_t *l, char *str)
 {
@@ -58,7 +81,50 @@ static int afd_parse_list(struct list_t *l, char *str)
 		p = strtok_r(NULL, " ", &save);
 	}
 
+#ifndef NDEBUG
 	list_map(l, afd_print_str);
+#endif
+	return 0;
+}
+
+static int afd_parse_tran_str(struct afd_tran *tran, char *str)
+{
+	char *save;
+	tran->ini = strtok_r(str, " ", &save);
+	tran->fin = strtok_r(NULL, " ", &save);
+	tran->sym = strtok_r(NULL, " ", &save);
+
+	return 0;
+}
+
+static int afd_parse_trans(struct list_t *l, char *str, char **save)
+{
+	char *p;
+	struct afd_tran *tran;
+	p = strtok_r(NULL, ";", save);
+
+	while(p != NULL)
+	{
+		debug("Nuevo elemento %s", p);
+		if((tran = malloc(sizeof(struct afd_tran))) == NULL)
+		{
+			debug("malloc");
+			return 1;
+		}
+
+		afd_parse_tran_str(tran, p);
+
+		if(list_add(l, tran) != 0)
+		{
+			debug("list_add");
+			return 1;
+		}
+
+		p = strtok_r(NULL, ";", save);
+	}
+#ifndef NDEBUG
+	list_map(l, afd_print_tran);
+#endif
 	return 0;
 }
 
@@ -74,6 +140,33 @@ static int afd_parse_sti(struct afd *afd, char *str)
 	}
 
 	afd->sti = p;
+
+	return 0;
+}
+
+static void afd_print_dot_tran(void *ptr)
+{
+	struct afd_tran *tran = (struct afd_tran *) ptr;
+	printf("\t\"%s\" -> \"%s\" [label = \"%s\"];\n",
+		tran->ini, tran->fin, tran->sym);
+}
+
+static void afd_print_dot_stf(void *ptr)
+{
+	char *stf = (char *) ptr;
+	printf("\t\"%s\" [peripheries = 2];\n", stf);
+}
+
+int afd_print_dot(struct afd *afd)
+{
+	printf("digraph G {\n");
+
+	list_map(&afd->trans, afd_print_dot_tran);
+	list_map(&afd->stf, afd_print_dot_stf);
+	printf("\tSTART_NODE [style = invis];\n");
+	printf("\tSTART_NODE -> \"%s\";\n", afd->sti);
+
+	printf("}\n");
 
 	return 0;
 }
@@ -111,6 +204,8 @@ int afd_parse(struct afd *afd)
 		return 1;
 	}
 
+	afd_parse_trans(&afd->trans, p, &save);
+
 	return 0;
 }
 
@@ -128,6 +223,8 @@ int main(int argc, char *argv[])
 		debug("afd_parse");
 		return 1;
 	}
+
+	afd_print_dot(afd);
 
 	afd_free(afd);
 
