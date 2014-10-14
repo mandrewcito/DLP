03d5404aefe47819a37cccdd087b9fccb08db2da
mandrewcito <anbaalo@gmail.com>
2014-10-14 16:19:01 +0200

    main de java y python modif , editado readme

--- java/src/afd/Main.java
+++ java/src/afd/Main.java
@@ -27,7 +27,7 @@ public class Main {
         }
 	}
 	public static void main(String[] args) {
-		Automata a = new Automata("conf-isla.txt");
+		Automata a = new Automata(args[0]);
 		System.out.println(a);
 		escribeFichero("conf-dot.txt","digraph{\ncompound=true;\n ",false);
 		escribeFichero("conf-dot.txt",a.generaDot("AFD"),true);
